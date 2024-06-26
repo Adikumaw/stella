7c7ec3e7c55717c65c859ad1ad28cad47374534f
all_father <kumawataditya105@gmail.com>
2024-06-27 00:15:46 +0530

    added repository anotation

--- src/main/java/com/nothing/ecommerce/repository/UpdateVerificationTokenRepository.java
+++ src/main/java/com/nothing/ecommerce/repository/UpdateVerificationTokenRepository.java
@@ -1,8 +1,11 @@
 package com.nothing.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.nothing.ecommerce.entity.UpdateVerificationToken;
 
+@Repository
 public interface UpdateVerificationTokenRepository extends JpaRepository<UpdateVerificationToken, Integer> {
     UpdateVerificationToken findByToken(String token);
 
--- src/main/java/com/nothing/ecommerce/repository/VerificationTokenRepository.java
+++ src/main/java/com/nothing/ecommerce/repository/VerificationTokenRepository.java
@@ -1,9 +1,11 @@
 package com.nothing.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.nothing.ecommerce.entity.VerificationToken;
 
+@Repository
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Integer> {
     VerificationToken findByToken(String token);
 
