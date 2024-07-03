ed5c8a17b422490cdc3ce07fe89eaa05a5aebae4
all_father <kumawataditya105@gmail.com>
2024-07-04 00:15:27 +0530

    changed role class

--- src/main/java/com/nothing/ecommerce/entity/Roles.java
+++ src/main/java/com/nothing/ecommerce/entity/Roles.java
@@ -2,6 +2,8 @@ package com.nothing.ecommerce.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -19,9 +21,18 @@ import lombok.ToString;
 @Table(name = "user_roles")
 public class Roles {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
     @Column(name = "user_id")
     private int userId;
 
     @Column(name = "role")
     private String role;
+
+    public Roles(int userId, String role) {
+        this.userId = userId;
+        this.role = role;
+    }
 }
